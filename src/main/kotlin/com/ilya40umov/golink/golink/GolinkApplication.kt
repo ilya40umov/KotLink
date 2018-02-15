3b93741033617c7fe23d1270feb30931bd1cad0f
Illia Sorokoumov <illia.sorokoumov@gmail.com>
2018-02-15 20:36:51 +0100

    Create basic Sprint Boot project

---
+++ src/main/kotlin/com/ilya40umov/golink/golink/GolinkApplication.kt
@@ -0,0 +1,11 @@
+package com.ilya40umov.golink.golink
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GolinkApplication
+
+fun main(args: Array<String>) {
+    runApplication<GolinkApplication>(*args)
+}
