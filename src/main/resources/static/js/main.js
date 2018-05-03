9720d1d36f7c2974c0617142674673f610d993eb
Illia Sorokoumov <illia.sorokoumov@olx.com>
2018-05-03 22:10:10 +0200

    Fix more js in terms of Codacy

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,4 @@
+/* global mdc */
 window.ktReady(function () {
-    if (!Boolean(window.mdc)) {
-        console.error("mdc is still not available!");
-        return;
-    }
     mdc.autoInit();
 });
