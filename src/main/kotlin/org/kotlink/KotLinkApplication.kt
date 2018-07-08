f92619e495d38d978cf1ebdba53075a069c72163
Illia Sorokoumov <illia.sorokoumov@gmail.com>
2018-07-08 11:21:10 +0200

    Fix tests (add unstaged files)

--- src/main/kotlin/org/kotlink/KotLinkApplication.kt
+++ src/main/kotlin/org/kotlink/KotLinkApplication.kt
@@ -2,9 +2,7 @@ package org.kotlink
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
 
-@EnableCaching
 @SpringBootApplication
 class KotLinkApplication
 
--- src/main/kotlin/org/kotlink/config/CachingConfig.kt
+++ src/main/kotlin/org/kotlink/config/CachingConfig.kt
@@ -1,2 +1,10 @@
 package org.kotlink.config
 
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@EnableCaching
+@Profile("!repotest")
+@Configuration
+class CachingConfig
