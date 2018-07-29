1b9cc220a637922a2d87e57fcbcd4cd8be83aa62
Illia Sorokoumov <illia.sorokoumov@gmail.com>
2018-07-29 18:41:29 +0200

    Implement equals on DatabaseException to allow 'shouldThrow' to handle it correctly

--- src/main/kotlin/org/kotlink/core/exposed/DatabaseException.kt
+++ src/main/kotlin/org/kotlink/core/exposed/DatabaseException.kt
@@ -1,3 +1,14 @@
 package org.kotlink.core.exposed
 
-class DatabaseException(message: String? = null, cause: Throwable? = null) : RuntimeException(message, cause)
+class DatabaseException(message: String? = null, cause: Throwable? = null) : RuntimeException(message, cause) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+}
